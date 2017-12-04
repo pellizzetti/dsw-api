07826201459940a94afe0f39ecb71400233ac766
Guilherme Pellizzetti <guipellizzetti@gmail.com>
2017-12-04 13:20:15 -0200

    Altera estrutura do projeto

--- src/main/java/br/udesc/ceavi/dsw/pfinal/TestService.java
+++ src/main/java/br/udesc/ceavi/dsw/pfinal/TestService.java
@@ -1,7 +1,9 @@
+package br.udesc.ceavi.dsw.pfinal;
+
 import static spark.Spark.get;
 
 public class TestService {
     public static void main( String[] args) {
-        get("/helloworld", (req, res) -> "Hello Sparkingly World!");
+        get("/helloworld", (req, res) -> "Feels bad, man...");
     }
 }
